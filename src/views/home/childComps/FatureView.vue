bf90f07674d5f8f1349b86238e2b0da01a8ebeec
hx0816 <879164392@qq.com>
2022-04-20 23:29:22 +0800

    recommend和fatureView完成

---
+++ src/views/home/childComps/FatureView.vue
@@ -0,0 +1,19 @@
+<template>
+  <div class="fature-view">
+      <img src="~@/assets/img/home/recommend_bg.jpg" alt="">
+  </div>
+</template>
+
+<script>
+export default {
+    name:'FatureView'
+}
+</script>
+
+<style scoped lang='scss'>
+.fature-view{
+    img{
+        width: 100%;
+    }
+}
+</style>
---
+++ src/views/home/childComps/Recommend.vue
@@ -0,0 +1,44 @@
+<template>
+  <div class="recommend" v-show="list.length!==0">
+    <div class="recommend-item" v-for="(item,index) in list" :key="index">
+      <img :src="item.image" />
+      <span>{{item.title}}</span>
+    </div>
+  </div>
+</template>
+
+<script>
+export default {
+  name: "Recommend",
+  props: {
+    list: {
+      type: Array,
+      default() {
+        return [];
+      }
+    }
+  }
+};
+</script>
+
+<style lang='scss' scoped>
+    .recommend{
+        margin-top: 15px;
+        padding-bottom: 20px;
+        border-bottom: 10px solid #eee;
+        display: flex;
+        &-item{
+            flex: 1;
+            display: flex;
+            flex-direction: column;
+            align-items: center;
+            img{
+                width: 80%;
+            }
+            span{
+                margin-top: 5px;
+                color: #aaaaaa;
+            }
+        }
+    }
+</style>
